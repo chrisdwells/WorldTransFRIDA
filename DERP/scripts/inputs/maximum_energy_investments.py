a5a805299a0104f10f2c86f489e971fd3d7bc360
chrisdwells <c.d.wells@leeds.ac.uk>
2025-03-31 08:28:34 +0100

    setup initial protocol with 20 members

--- DERP/scripts/inputs/maximum_energy_investments.py
+++ DERP/scripts/inputs/maximum_energy_investments.py
@@ -7,7 +7,8 @@ nt = y2 - y1 + 1
 
 df_in = pd.read_csv('../../data/intermediate_outputs/baseline_energy_investments_by_source.csv')
 
-base_scenario = 'Baseline'
+n_members = 20
+
 
 sources_damaged = ['Coal energy conversion', 'Coal fuel extraction', 
                    'Oil energy conversion', 'Oil fuel extraction', 
@@ -23,16 +24,12 @@ data_dict = {
     'Years':np.arange(y1, y2+1, 1),
     }
 
-for source in sources_damaged:
-    data_dict[f'energy investments.maximum investments in {source}'
-              ] = df_in[f'{base_scenario}: energy investments.investment by source[1, {source}]']
+for n in np.arange(n_members):
+    for source in sources_damaged:
+        data_dict[f'energy investments.maximum investments in {source}[{n+1}]'
+                  ] = df_in[f'Run {n+1}: energy investments.investment by source[1, {source}]']
 
 df_frida = pd.DataFrame(data_dict)
 
 df_frida = df_frida.set_index('Years')
 df_frida.to_csv('../../data/inputs/energy_investments_max_by_source.csv')
-
-
-
-#%%
-
